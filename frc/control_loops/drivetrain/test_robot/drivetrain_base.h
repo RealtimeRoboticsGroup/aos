b8f5b1a004f4d1359ac2adf4f48a5864ef95d751
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2025-02-01 16:36:23 -0800

    Delete y2019/ folder
    Start working on removing sundry robot-specific code.
    
    Change-Id: Id10ebd61e7996bc8cebff037f19d4644e786bfa4
    Signed-off-by: James Kuszmaul <jabukuszmaul+collab@gmail.com>

---
+++ frc/control_loops/drivetrain/test_robot/drivetrain_base.h
@@ -0,0 +1,13 @@
+#ifndef Y2016_CONTROL_LOOPS_DRIVETRAIN_DRIVETRAIN_BASE_H_
+#define Y2016_CONTROL_LOOPS_DRIVETRAIN_DRIVETRAIN_BASE_H_
+
+#include "frc/control_loops/drivetrain/drivetrain_config.h"
+
+namespace frc::control_loops::drivetrain::test_robot {
+
+const ::frc::control_loops::drivetrain::DrivetrainConfig<double> &
+GetDrivetrainConfig();
+
+}  // namespace frc::control_loops::drivetrain::test_robot
+
+#endif  // Y2016_CONTROL_LOOPS_DRIVETRAIN_DRIVETRAIN_BASE_H_
