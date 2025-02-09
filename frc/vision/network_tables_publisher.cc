0ef569c66e33d5471fdea12ac05e82cf8227ccae
Austin Schuh <austin.linux@gmail.com>
2025-02-09 15:53:22 -0800

    Add rudimentary network tables publisher
    This attempts to implement the limelight API, though it doesn't handle
    multiple tags, and doesn't do the translation from camera coordinates to
    field coordinates.
    
    Change-Id: Ib936a4af68f82ea0a22206c46db6837ba2ab2d89
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

---
+++ frc/vision/network_tables_publisher.cc
@@ -0,0 +1,112 @@
+#include <networktables/DoubleArrayTopic.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+
+#include "Eigen/Core"
+#include "Eigen/Geometry"
+#include "absl/flags/flag.h"
+
+#include "aos/configuration.h"
+#include "aos/events/shm_event_loop.h"
+#include "aos/init.h"
+#include "frc/vision/target_map_generated.h"
+
+ABSL_FLAG(std::string, config, "aos_config.json",
+          "File path of aos configuration");
+
+namespace frc::vision {
+
+class NetworkTablesPublisher {
+ public:
+  NetworkTablesPublisher(aos::EventLoop *event_loop,
+                         std::string_view table_name)
+      : event_loop_(event_loop),
+        table_(nt::NetworkTableInstance::GetDefault().GetTable(table_name)),
+        pose_topic_(table_->GetDoubleArrayTopic("botpose_wpiblue")) {
+    for (size_t i = 0; i < 4; i++) {
+      event_loop_->MakeWatcher(absl::StrCat("/camera", i, "/gray"),
+                               [this, i](const TargetMap &target_map) {
+                                 HandleTargetMap(i, target_map);
+                               });
+    }
+  }
+
+ private:
+  void HandleTargetMap(int i, const TargetMap &target_map) {
+    VLOG(1) << "Got map for " << i;
+    if (target_map.target_poses()->size() == 0) {
+      Publish(Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero(), 0, 0, 0, 0, 0);
+      return;
+    }
+
+    // TODO(austin): What do we do with multiple targets?  Need to fuse them
+    // somehow.
+    const TargetPoseFbs *target_pose = target_map.target_poses()->Get(0);
+
+    const Eigen::Vector3d translation(target_pose->position()->x(),
+                                      target_pose->position()->y(),
+                                      target_pose->position()->z());
+
+    const Eigen::Quaternion<double> orientation(
+        target_pose->orientation()->w(), target_pose->orientation()->x(),
+        target_pose->orientation()->y(), target_pose->orientation()->z());
+
+    // TODO(austin): Is this the right set of euler angles?
+    const Eigen::Vector3d ypr =
+        orientation.toRotationMatrix().eulerAngles(0, 1, 2);
+
+    const double age_ms =
+        std::chrono::duration<double, std::milli>(
+            event_loop_->monotonic_now() -
+            aos::monotonic_clock::time_point(
+                std::chrono::nanoseconds(target_map.monotonic_timestamp_ns())))
+            .count();
+
+    Publish(translation, ypr, age_ms, target_map.target_poses()->size(), 0,
+            translation.norm(), 0);
+  }
+
+  void Publish(Eigen::Vector3d translation, Eigen::Vector3d ypr,
+               double latency_ms, int tag_count, double tag_span_m,
+               double tag_dist_m, double tag_area_percent) {
+    nt::DoubleArrayPublisher publisher =
+        pose_topic_.Publish({.keepDuplicates = true});
+
+    std::array<double, 11> pose{
+        translation.x(),  translation.y(),
+        translation.z(),  ypr.x(),
+        ypr.y(),          ypr.z(),
+        latency_ms,       static_cast<double>(tag_count),
+        tag_span_m,       tag_dist_m,
+        tag_area_percent,
+    };
+
+    publisher.Set(pose);
+  }
+
+  aos::EventLoop *event_loop_;
+
+  std::shared_ptr<nt::NetworkTable> table_;
+  nt::DoubleArrayTopic pose_topic_;
+};
+
+int Main() {
+  aos::FlatbufferDetachedBuffer<aos::Configuration> config =
+      aos::configuration::ReadConfig(absl::GetFlag(FLAGS_config));
+
+  aos::ShmEventLoop event_loop(&config.message());
+
+  NetworkTablesPublisher publisher(&event_loop, "orin");
+
+  event_loop.Run();
+
+  return 0;
+}
+
+}  // namespace frc::vision
+
+int main(int argc, char **argv) {
+  aos::InitGoogle(&argc, &argv);
+
+  return frc::vision::Main();
+}
