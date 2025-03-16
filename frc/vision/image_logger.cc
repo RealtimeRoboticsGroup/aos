a309751ac8a427f0882bffc2fb49514676c82ec1
Austin Schuh <austin.linux@gmail.com>
2025-03-16 16:55:30 -0700

    Rename logs during official matches with matchnames
    This helps enormously at finding logs.  Relies on data pulled from
    network tables in network_tables_swerve_client for the event name and
    everything.
    
    Change-Id: I95e16ef0084dd71b7898f5e615f4ca92837a8664
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- frc/vision/image_logger.cc
+++ frc/vision/image_logger.cc
@@ -23,12 +23,11 @@ ABSL_FLAG(double, disabled_time, 5.0,
 ABSL_FLAG(bool, direct, false,
           "If true, write using O_DIRECT and write 512 byte aligned blocks "
           "whenever possible.");
+ABSL_FLAG(bool, always_log, false,
+          "If true, ignore the disabled signal and log all the time.");
 
 std::unique_ptr<aos::logger::MultiNodeFilesLogNamer> MakeLogNamer(
-    aos::EventLoop *event_loop) {
-  std::optional<std::string> log_name =
-      aos::logging::MaybeGetLogName("image_log");
-
+    aos::EventLoop *event_loop, std::optional<std::string> log_name) {
   if (!log_name.has_value()) {
     return nullptr;
   }
@@ -38,6 +37,74 @@ std::unique_ptr<aos::logger::MultiNodeFilesLogNamer> MakeLogNamer(
       std::make_unique<aos::logger::RenamableFileBackend>(
           absl::StrCat(log_name.value(), "/"), absl::GetFlag(FLAGS_direct)));
 }
+
+struct LogName {
+  std::string MakeName() const {
+    if (match_number != 0 && replay_number != 0 &&
+        (match_type == frc::MatchType::kQualification ||
+         match_type == frc::MatchType::kElimination)) {
+      std::string match_type_string =
+          (match_type == frc::MatchType::kQualification ? "q" : "e");
+      std::string event_name_prepend;
+
+      if (!event_name.empty()) {
+        event_name_prepend = absl::StrCat(event_name, "-");
+      }
+
+      if (replay_number == 1) {
+        return absl::StrCat(log_name, "-", event_name_prepend,
+                            match_type_string, match_number, "/");
+      } else {
+        return absl::StrCat(log_name, "-", event_name_prepend,
+                            match_type_string, match_number, "-r",
+                            replay_number, "/");
+      }
+    }
+    return absl::StrCat(log_name, "/");
+  }
+
+  void Reset(std::string new_log_name) {
+    match_number = -1;
+    replay_number = -1;
+    match_type = frc::MatchType::kNone;
+    log_name = new_log_name;
+    event_name = "";
+  }
+
+  std::string log_name;
+  int match_number = -1;
+  int replay_number = -1;
+  frc::MatchType match_type = frc::MatchType::kNone;
+  std::string event_name;
+
+  bool Update(const frc::JoystickState &joystick_state) {
+    bool update = false;
+    if (joystick_state.match_number() != match_number && match_number != 0) {
+      update = true;
+    }
+    match_number = joystick_state.match_number();
+
+    if (joystick_state.replay_number() != replay_number && replay_number != 0) {
+      update = true;
+    }
+    replay_number = joystick_state.replay_number();
+
+    if (joystick_state.match_type() != match_type &&
+        (match_type == frc::MatchType::kQualification ||
+         match_type == frc::MatchType::kElimination)) {
+      update = true;
+    }
+    match_type = joystick_state.match_type();
+
+    if (joystick_state.has_event_name() &&
+        joystick_state.event_name()->string_view() != event_name) {
+      event_name = joystick_state.event_name()->string_view();
+      update = true;
+    }
+
+    return update;
+  }
+};
 
 int main(int argc, char *argv[]) {
   absl::SetProgramUsageMessage(
@@ -82,15 +149,22 @@ int main(int argc, char *argv[]) {
     PCHECK(errno == 0) << ": Renicing to -20 failed.";
   });
 
+  LogName log_name_accumulator;
+  aos::logger::MultiNodeFilesLogNamer *current_log_namer = nullptr;
+
   event_loop.MakeWatcher("/frc", [&](const frc::JoystickState &joystick_state) {
     const auto timestamp = event_loop.context().monotonic_event_time;
     filesystem_status.Fetch();
+    bool joystick_state_enabled = joystick_state.enabled();
+    if (absl::GetFlag(FLAGS_always_log)) {
+      joystick_state_enabled = true;
+    }
 
     // Store the last time we got disabled
-    if (enabled && !joystick_state.enabled()) {
+    if (enabled && !joystick_state_enabled) {
       last_disable_time = timestamp;
     }
-    enabled = joystick_state.enabled();
+    enabled = joystick_state_enabled;
 
     bool enough_space = true;
 
@@ -114,22 +188,35 @@ int main(int argc, char *argv[]) {
         enough_space;
 
     if (!logging && should_be_logging) {
-      auto log_namer = MakeLogNamer(&event_loop);
+      std::optional<std::string> log_name =
+          aos::logging::MaybeGetLogName("image_log");
+      auto log_namer = MakeLogNamer(&event_loop, log_name);
       if (log_namer == nullptr) {
         return;
       }
+      current_log_namer = log_namer.get();
 
       // Start logging if we just got enabled
-      LOG(INFO) << "Starting logging";
+      LOG(INFO) << "Starting logging to " << log_namer->base_name();
       logger.StartLogging(std::move(log_namer));
       logging = true;
       last_rotation_time = event_loop.monotonic_now();
+      log_name_accumulator.Reset(log_name.value());
     } else if (logging && !should_be_logging) {
       // Stop logging if we've been disabled for a non-negligible amount of
       // time
       LOG(INFO) << "Stopping logging";
       logger.StopLogging(event_loop.monotonic_now());
       logging = false;
+      current_log_namer = nullptr;
+    }
+
+    // It is pretty cheap to rename, rename if the name gets "better" than
+    // before.
+    if (logging) {
+      if (log_name_accumulator.Update(joystick_state)) {
+        current_log_namer->set_base_name(log_name_accumulator.MakeName());
+      }
     }
   });
 
