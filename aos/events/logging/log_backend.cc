c0d5ff2f11998ad7fe524ad1311d845f377b6473
Austin Schuh <austin.linux@gmail.com>
2026-01-02 17:48:45 -0800

    OSX Sync implementation
    Change-Id: I0d9aedadd57855f7e6ab695a53ba892bca0cfc53

--- aos/events/logging/log_backend.cc
+++ aos/events/logging/log_backend.cc
@@ -180,6 +180,14 @@ void FileHandler::EnableDirect() {
       odirect_enabled_ = true;
       VLOG(1) << "Enabled O_DIRECT on " << filename_;
     }
+#elif defined(__APPLE__)
+    if (fcntl(fd_, F_NOCACHE, 1) == -1) {
+      PLOG(WARNING) << "Failed to set F_NOCACHE on " << filename_;
+      supports_odirect_ = false;
+    } else {
+      odirect_enabled_ = true;
+      VLOG(1) << "Enabled F_NOCACHE on " << filename_;
+    }
 #else
     // OSX likes aligned blocks to write efficiently, but does it implicitly rather
     // than explicitly.
@@ -194,6 +202,8 @@ void FileHandler::DisableDirect() {
 #ifdef O_DIRECT
     flags_ = flags_ & (~O_DIRECT);
     PCHECK(fcntl(fd_, F_SETFL, flags_) != -1) << ": Failed to disable O_DIRECT";
+#elif defined(__APPLE__)
+    PCHECK(fcntl(fd_, F_NOCACHE, 0) != -1) << ": Failed to disable F_NOCACHE";
 #endif
     odirect_enabled_ = false;
     VLOG(1) << "Disabled O_DIRECT on " << filename_;
@@ -318,6 +328,7 @@ WriteCode FileHandler::WriteV(const std::vector<struct iovec> &iovec,
   }
 
   if (absl::GetFlag(FLAGS_sync)) {
+#ifdef __linux__
     // Flush asynchronously and force the data out of the cache.
     sync_file_range(fd_, total_write_bytes_, written, SYNC_FILE_RANGE_WRITE);
     if (last_synced_bytes_ != 0) {
@@ -333,6 +344,11 @@ WriteCode FileHandler::WriteV(const std::vector<struct iovec> &iovec,
                     total_write_bytes_ - last_synced_bytes_,
                     POSIX_FADV_DONTNEED);
     }
+#elif defined(__APPLE__)
+    if (fcntl(fd_, F_FULLFSYNC) == -1) {
+      PLOG(WARNING) << "Failed to F_FULLFSYNC " << filename_;
+    }
+#endif
     last_synced_bytes_ = total_write_bytes_;
   }
 
@@ -352,7 +368,15 @@ WriteCode FileHandler::Close() {
 
   if (absl::GetFlag(FLAGS_sync)) {
     // Force everythig out at the end so we know that it hits disk.
+#ifdef __linux__
     fdatasync(fd_);
+#elif defined(__APPLE__)
+    if (fcntl(fd_, F_FULLFSYNC) == -1) {
+      PLOG(WARNING) << "Failed to F_FULLFSYNC " << filename_;
+    }
+#else
+    fsync(fd_);
+#endif
   }
 
   if (close(fd_) == -1) {
