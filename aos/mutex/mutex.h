b17d4372e3ea51eaa5cf0de70fa817c29994a4c4
Austin Schuh <austin.linux@gmail.com>
2026-01-04 00:25:06 -0800

    Reduce gratutious changes
    Change-Id: I9d1e477532bc91e454f40ad2359fe26a8b9b7c50

--- aos/mutex/mutex.h
+++ aos/mutex/mutex.h
@@ -6,12 +6,9 @@
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 
+#include "aos/ipc_lib/aos_sync.h"
 #include "aos/macros.h"
 #include "aos/type_traits/type_traits.h"
-#include <mutex>
-#include <thread>
-
-#include "aos/ipc_lib/aos_sync.h"
 
 namespace aos {
 // An abstraction of a mutex that is easy to implement for environments other
@@ -60,10 +57,6 @@ class Mutex {
   // Returns true iff the current task has this mutex locked.
   // This is mainly for IPCRecursiveMutexLocker to use.
   bool OwnedBySelf() const;
-
-  // BasicLockable compatibility for std::condition_variable_any
-  void lock() { (void)Lock(); }
-  void unlock() { Unlock(); }
 
  private:
   aos_mutex impl_;
