44d961d7cad543fc9dde1dfd801c013a614c00ea
Austin Schuh <austin.linux@gmail.com>
2026-07-08 15:52:11 -0700

    Update compiler_memory_barrier.h for windows
    Change-Id: Icd562f61f5ddfdc7957d1537ae33cca3c5671fb6
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- aos/util/compiler_memory_barrier.h
+++ aos/util/compiler_memory_barrier.h
@@ -4,8 +4,14 @@
 // Prevents the compiler from reordering memory operations around this.
 // Using this function makes it clearer what you're doing and easier to be
 // portable.
+#ifdef _MSC_VER
+#include <intrin.h>
+#pragma intrinsic(_ReadWriteBarrier)
+static inline void aos_compiler_memory_barrier(void) { _ReadWriteBarrier(); }
+#else
 static inline void aos_compiler_memory_barrier(void) {
   __asm__ __volatile__("" ::: "memory", "cc");
 }
+#endif
 
 #endif  // AOS_UTIL_COMPILER_MEMORY_BARRIER_H_
